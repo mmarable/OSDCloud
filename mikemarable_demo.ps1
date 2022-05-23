55baa73cf6bbe062f547dee0251a0150f7958fa2
mmarable <mmarable2004@hotmail.com>
2022-05-23 12:41:51 -0400

    Update mikemarable_demo.ps1

---
+++ mikemarable_demo.ps1
@@ -0,0 +1,13 @@
+Write-Host "This is a test" -ForegroundColor Green
+# =============================================================================
+# Features to Demonstrate
+# Key Vault
+#   We can use this to secure the MAK key
+# Cloud Scripts (this script)
+# OOBE
+#   Autopilot Register (registered application)
+#   Time Zone setting (steal from David)
+#   Patching
+# Other?
+# =============================================================================
+
