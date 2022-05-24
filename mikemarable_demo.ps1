eee1050e0f4f0327651babbc6e41391333f9f0e6
mmarable <mmarable2004@hotmail.com>
2022-05-24 09:33:58 -0400

    Update mikemarable_demo.ps1

--- mikemarable_demo.ps1
+++ mikemarable_demo.ps1
@@ -50,7 +50,7 @@ Param()
 
 #----------------------------
 # Initialize
-Write-Host -ForegroundColor DarkGray "OSDCloud Demo 22.5.23.1"
+Write-Host -ForegroundColor DarkGray "OSDCloud Demo 22.5.24.1"
 Invoke-Expression -Command (Invoke-RestMethod -Uri functions.osdcloud.com)
 $Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-OSDCloud-MikeMarable.log"
 $null = Start-Transcript -Path (Join-Path "$env:SystemRoot\Temp" $Transcript) -ErrorAction Ignore
